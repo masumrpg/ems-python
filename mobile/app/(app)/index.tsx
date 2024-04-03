cce386028820c0bef7e19ddbc150f88c347acbf8
Masum <masumrpg@gmail.com>
2024-04-03 15:47:06 +0700

    Backend: - fix prefix endpoint - fix getAll attendance Mobile: - add mobile client

---
+++ mobile/app/(app)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+    return (
+        <View className={"bg-red-200 pt-14"}>
+            <Text className={"text-center text-3xl"}>Home</Text>
+        </View>
+    );
+}
